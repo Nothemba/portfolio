70ece84ebfbb828258a50f2d00c3b1bdaf2a27b7
Nothemba Tsheme <tnothemba@gmail.com>
2019-05-23 14:13:52 +0200

    changed the colors of a website and re-aligned the menu

---
+++ Untitled-1.js
@@ -0,0 +1,16 @@
+var num1 =5;
+var num2 = 8;
+var sum = num1 + num2;
+
+if(num1 === 3 || num2 === 3){
+return true;
+
+}
+var arr =[];
+arr = sum;
+console.log(arr);
+/* sum.split(" ");
+for(var i = 0;i< sum;i++){
+sum 
+
+}*/
